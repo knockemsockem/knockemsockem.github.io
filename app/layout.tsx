d65fcde2471eecacfa31da7b0e0987ad88ac05a9
aiiey <i@clu.ng>
2024-12-30 13:25:46 -0600

    domain switch

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,15 +7,15 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "@decal | clu.ng",
+  title: "@decal | dec.al",
   description: "Private Biolink developed with Next.js | @x05k on ig",
   icons: {
     icon: "https://cdn.discordapp.com/emojis/1310254577831903254.webp?size=128",
   },
   openGraph: {
-    title: "@decal | clu.ng",
+    title: "@decal | dec.al",
     description: "Private Biolink developed with Next.js | @x05k on ig",
-    url: "https://clu.ng",
+    url: "https://dec.al",
     images: [
       {
         url: "https://i.pinimg.com/736x/9f/c7/e3/9fc7e3ec06cd28eb55b3bc8a0663cb69.jpg", // Replace with your thumbnail URL
