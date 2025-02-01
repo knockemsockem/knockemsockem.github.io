f5bdb5a69e2c09b92b81c25bc1fcc79dd78d8edc
aiiey <i@clu.ng>
2025-01-31 23:21:53 -0600

    hi

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ const Framed = () => {
         playsInline
         className="fixed inset-0 object-cover z-0 w-full h-full"
       >
-        <source src="/0131.mp4" type="video/mp4" />
+        <source src="https://files.catbox.moe/zs8dsr.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <ThreeDCardDemo />
